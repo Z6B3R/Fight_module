8c8407c33634f60344bb94494f097e4bfabb4196
Z6B3R <Z6B3Rint@yandex.ru>
2024-05-03 16:27:52 +0300

    Некоторые изменения функций

--- Fight_module_analog.cpp
+++ Fight_module_analog.cpp
@@ -6,7 +6,7 @@ struct player
 {
     std::string name, tag;
     int HP = -1, AC = -1, IP = -1;
-    void set(std::string utag, std::string uname, int uhp, int uac, int uip)
+    void set_stats(std::string utag, std::string uname, int uhp, int uac, int uip)
     {
         name = uname;
         HP = uhp;
@@ -14,7 +14,7 @@ struct player
         IP = uip;
         tag = utag;
     }
-    void show_players_info(std::vector<player> &party)
+    void players_show_info(std::vector<player> &party)
     {
         for (int x = 0; x < party.size(); ++x)
         {
@@ -22,9 +22,16 @@ struct player
         }
         std::cout << "__________________" << std::endl;
     }
+    void hp_damage()
+    {
+        //получение урона
+    }
+    void hp_heal()
+    {
+        //лечение
+    }
 };
-
-bool create_member(player &mem, std::vector<player> &fight)
+void init_create_member(player &mem, std::vector<player> &fight) //возможно переработать
 {
     std::string name; int hp = 0, ac = 0, ip = 0;
     int num = 0;
@@ -36,7 +43,7 @@ bool create_member(player &mem, std::vector<player> &fight)
         std::cout << "\nВведи хиты: "; std::cin >> hp;
         std::cout << "\nВведи очки брони: "; std::cin >> ac;
         std::cout << "\nВведи очки инициативы: "; std::cin >> ip;
-        mem.set(tag, name, hp, ac, ip);
+        mem.set_stats(tag, name, hp, ac, ip);
         fight.push_back(mem);
     }
     std::cout << "Введи количество противников: "; std::cin >> num;
@@ -47,12 +54,11 @@ bool create_member(player &mem, std::vector<player> &fight)
         std::cout << "\nВведи хиты: "; std::cin >> hp;
         std::cout << "\nВведи очки брони: "; std::cin >> ac;
         std::cout << "\nВведи очки инициативы: "; std::cin >> ip;
-        mem.set(tag, name, hp, ac, ip);
+        mem.set_stats(tag, name, hp, ac, ip);
         fight.push_back(mem);
     }
-    return true;
 }
-bool well_sort_IP(std::vector<player> &party)
+bool init_sort_IP(std::vector<player> &party)
 {
     bool check_sort = false;
     do
@@ -66,7 +72,7 @@ bool well_sort_IP(std::vector<player> &party)
                 std::swap(party[x], party[x + 1]);
                 ++x;
             }
-            else if (party[x].IP == party[x + 1].IP && party[x].tag == "npc")
+            else if (party[x].IP == party[x + 1].IP && party[x].tag == "npc") //если числа одинаковы, чтобы первым ходил игрок
             {
                 std::swap(party[x], party[x + 1]);
                 ++x;
@@ -74,6 +80,7 @@ bool well_sort_IP(std::vector<player> &party)
             else
                 ++x;
         }
+        //проверка убывания
         for (int x = 0; x < party.size(); ++x)
         {
             if (x + 1 >= party.size())
@@ -93,13 +100,17 @@ bool well_sort_IP(std::vector<player> &party)
     } while (check_sort == false);
     return true;
 }
+bool fight_stop(std::vector<player> &party)
+{
+ 
+}
 int main()
 {
     setlocale(LC_ALL, "");
     player newmem;
     std::vector<player>fight_party;
-    create_member(newmem, fight_party);
-    newmem.show_players_info(fight_party);
-    well_sort_IP(fight_party);
-    newmem.show_players_info(fight_party);
+    init_create_member(newmem, fight_party);
+    init_sort_IP(fight_party);
+    newmem.players_show_info(fight_party);
+
 }
